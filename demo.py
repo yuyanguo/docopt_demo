2fe480319f9b28466bdcdfddbec8ea865e145635
Yuyan Guo <yuyanguo@student.ubc.ca>
2020-11-21 17:56:16 -0500

    Modify details for demo.py

--- demo.py
+++ demo.py
@@ -6,7 +6,7 @@ Usage: docopt.py <arg1> [<arg4>] --arg2=<arg2> [--arg3=<arg3>]
 
 Options:
 <arg>             Takes any value (this is a required positional argument)
-[<arg4>]          Takes any value (this is a required positional argument)
+[<arg4>]          Takes any value (this is a optional positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
 """
